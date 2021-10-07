1f513cda7a0548206b0aa182c07261665c53a43f
Triana Cerda <trianaRcerda@gmail.com>
2021-10-07 16:43:58 -0700

    failing test for /post route for login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const usersService = require('../lib/services/usersService');
 
 describe('auth-lab routes', () => {
   beforeEach(() => {
@@ -10,11 +11,23 @@ describe('auth-lab routes', () => {
   jest.setTimeout(10000);
 
   const userT = { email: 'tri@ana.com', password: 'rainman' };
+  const userJ = { email: 'jon@athan.com', password: 'brotherRAM' };
 
   it('should sign up a new user with a POST ', async () => {
     const res = await request(app).post('/api/v1/auth/sign-up').send(userT);
 
     expect(res.body).toEqual({ id: expect.any(String), email: 'tri@ana.com' });
+  });
+
+  it('logs in a user via POST', async () => {
+    await usersService.createUser(userT);
+
+    const res = await request(app).post('/api/v1/auth/login').send(userJ);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'jon@athan.com',
+    });
   });
 
   afterAll(() => {
