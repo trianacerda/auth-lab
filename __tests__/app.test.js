f30c98086092aea8a40df78ada422cd3ab931979
Triana Cerda <trianaRcerda@gmail.com>
2021-10-07 13:48:59 -0700

    failing test for /post signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('auth-lab routes', () => {
     return setup(pool);
   });
 
+  const userT = { email: 'tri@ana.com', password: 'rainman' };
+
+  it('should sign up a new user with a POST ', async () => {
+    return await request(app)
+      .post('/api/vi/auth/signup')
+      .send(userT)
+      .then((res) => {
+        expect(res.body).toEqual({ ...userT, id: expect.any(String) });
+      });
+  });
+
   afterAll(() => {
     pool.end();
   });
